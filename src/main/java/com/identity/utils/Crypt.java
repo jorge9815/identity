4cb384c4e53b64afbcf0e7f36f7f1336abcf4d23
Dany Jimenez <dejimenez86@gmail.com>
2021-10-17 19:21:21 +0200

    Add crypt util

---
+++ src/main/java/com/identity/utils/Crypt.java
@@ -0,0 +1,11 @@
+package com.identity.utils;
+
+public class Crypt {
+    public static String encode(String text) {
+        return text;
+    }
+
+    public static boolean verify(String text) {
+        return true;
+    }
+}
