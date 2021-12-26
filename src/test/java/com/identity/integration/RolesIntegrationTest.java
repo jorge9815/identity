835039f4c88bb18c76b36567649be34f95e1bc61
Jorge <jorge9815rdz@gmail.com>
2021-12-26 18:14:00 -0500

    configuring for integration test

--- src/test/java/com/identity/integration/RolesIntegrationTest.java
+++ src/test/java/com/identity/integration/RolesIntegrationTest.java
@@ -1,56 +1,64 @@
-//package com.identity.integration;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.fasterxml.jackson.databind.ObjectWriter;
-//import com.identity.TestData;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//public class RolesIntegrationTest {
-//    @Autowired
-//    private MockMvc mvc;
-//    private Data data = new Data();
-//    private String token = "Bearer ";
-//    private final String tokenName = "Authorization";
-//
-//    @BeforeEach
-//    public void beforeEach() throws Exception {
-//        token += data.generateToken();
-//    }
-//
-//    @Test
-//    public void getAllRoles() throws Exception {
-//        mvc.perform(get("/roles?page=1&perPage=4")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .header("Authorization", "Bearer " + token)
-//                )
-//                .andExpect(status().isOk())
-//                .andExpect(content()
-//                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
-//    }
-//
-//    @Test
-//    public void SaveRoleTest() throws Exception {
-//        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-//
-//        mvc.perform(post("/roles")
-//                        .header("Authorization", "Bearer " + token)
-//                        .contentType(MediaType.APPLICATION_JSON)
-////                .param(ow.writeValueAsString(TestData.getRoleDto()))
-//                        .contentType(ow.writeValueAsString(TestData.getRoleDto()))
-//        );
-//    }
-//}
-//
+package com.identity.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.identity.TestData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+public class RolesIntegrationTest {
+    @Autowired
+    private MockMvc mvc;
+    private Data data = new Data();
+    private String token = "Bearer ";
+    private final String tokenName = "Authorization";
+
+    @BeforeEach
+    public void beforeEach() throws Exception {
+        token += data.generateToken();
+    }
+
+    @Test
+    public void getAllRoles() throws Exception {
+        mvc.perform(get("/roles?page=1&perPage=4")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", token)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    public void SaveRoleTest() throws Exception {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+
+        mvc.perform(post("/roles")
+                        .header("Authorization", token)
+                        .contentType(MediaType.APPLICATION_JSON)
+//                .param(ow.writeValueAsString(TestData.getRoleDto()))
+                        .contentType(ow.writeValueAsString(TestData.getRoleDto()))
+        );
+    }
+}
+
