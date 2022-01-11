760601cfa83f8f2407a326716d133a58f2f17f5e
Jorge <jorge9815rdz@gmail.com>
2022-01-11 09:24:10 -0500

    added E2E test for roles modules

--- src/test/java/com/identity/integration/RolesIntegrationTest.java
+++ src/test/java/com/identity/integration/RolesIntegrationTest.java
@@ -5,39 +5,50 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 import com.identity.TestData;
 import com.identity.roles.aplication.RoleDto;
 import com.identity.shared.PaginatedList;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import java.util.List;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class RolesIntegrationTest {
     @Autowired
     private MockMvc mvc;
     private String token = "Bearer ";
     private final String tokenName = "Authorization";
-    private RoleDto role = TestData.getRoleDto();
+    private RoleDto role=TestData.getRoleDto();
     ObjectWriter ow = new ObjectMapper().writer()
             .withDefaultPrettyPrinter();
 
     @BeforeEach
     public void beforeEach() throws Exception {
         token += TestData.generateToken();
+//        role =TestData.getRoleDto();
+
     }
+
+//    @AfterEach
+//    public void afterEach() throws Exception {
+//        deleteRole();
+//    }
 
     @Test
     public void getAllRoles() throws Exception {
@@ -47,7 +58,7 @@ public class RolesIntegrationTest {
                 .andExpect(content()
                         .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(content().json(ow
-                        .writeValueAsString(new PaginatedList<RoleDto>(1l, 1,4,List.of(role)))));
+                        .writeValueAsString(new PaginatedList<RoleDto>(1l, 1, 4, List.of(role)))));
     }
 
     @Test
@@ -59,8 +70,8 @@ public class RolesIntegrationTest {
     public void GetByNameTest() throws Exception {
         saveRole(role);
         mvc.perform(configureRequest(get("/role?name=" + role.getName()))
-        ).andExpect(content()
-                .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                ).andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(content().json(ow.writeValueAsString(role))
                 );
     }
@@ -68,23 +79,50 @@ public class RolesIntegrationTest {
     @Test
     public void getByIdTest() throws Exception {
         saveRole(role);
-        mvc.perform(configureRequest(get("/role/"+role.getId())))
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+        getByID()
                 .andExpect(content().json(ow.writeValueAsString(role)));
     }
 
 
+    @Test
+    public void deleteRoleTest() throws Exception {
+        saveRole(role);
+        deleteRole();
+    }
+
+    @Test
+    public void updateRoleTest() throws Exception {
+
+        saveRole(role);
+        role.setName("Admin2");
+        mvc.perform(configureRequest(put("/roles/update"))
+                        .content(ow.writeValueAsString(role)))
+                .andExpect(status().isOk());
+        getByID()
+                .andExpect(content().json(ow.writeValueAsString(role)));
+    }
+
+    private void deleteRole() throws Exception {
+        mvc.perform(configureRequest(delete("/role/" + role.getId())))
+                .andExpect(status().isOk());
+    }
 
     private void saveRole(RoleDto role) throws Exception {
         mvc.perform(configureRequest(post("/roles"))
                 .content(ow.writeValueAsString(role))
-        );
+
+        ).andExpect(status().isOk());
     }
 
     private MockHttpServletRequestBuilder configureRequest(MockHttpServletRequestBuilder httpMethod) {
         return httpMethod
                 .contentType(MediaType.APPLICATION_JSON)
                 .header(tokenName, token);
+    }
+
+    private ResultActions getByID() throws Exception {
+        return mvc.perform(configureRequest(get("/role/" + role.getId())))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
     }
 
 }
